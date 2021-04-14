d5f7681be149ff0e9a5bb61c6ee06ea77ceccde9
Yurii <urarat271@gmail.com>
2021-04-14 15:07:35 +0300

    fix Parser.java

--- src/main/java/ru/dismals/diplom/service/Parser.java
+++ src/main/java/ru/dismals/diplom/service/Parser.java
@@ -1,5 +1,6 @@
 package ru.dismals.diplom.service;
 
+import org.apache.commons.io.IOUtils;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.springframework.stereotype.Service;
@@ -14,6 +15,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -26,7 +28,12 @@ public class Parser {
     private final String BASE_URL = "https://ourworldindata.org/grapher/data/variables/" +
             "8841+8840+8838+8837+8834+8833+8815+8814+8813+8812+8811+8807+8804+8806.json?v=2";
 
-    public Parser(ProductRepo productRepo, YearRepo yearRepo) {
+    private final ProductRepo productRepo;
+    private final YearRepo yearRepo;
+
+    public Parser(ProductRepo productRepo, YearRepo yearRepo, ProductRepo productRepo1, YearRepo yearRepo1) {
+        this.productRepo = productRepo1;
+        this.yearRepo = yearRepo1;
     }
 
     @PostConstruct
@@ -37,29 +44,32 @@ public class Parser {
         InputStream inputStream = connection.getInputStream();
         BufferedInputStream bufferedInputStream = new BufferedInputStream(inputStream);
 
-        JSONObject jsonObject = new JSONObject(bufferedInputStream);
+        JSONObject jsonObject = new JSONObject(IOUtils.toString(bufferedInputStream, StandardCharsets.UTF_8.toString()));
         JSONObject variables = jsonObject.getJSONObject("variables");
 
-        List<YearAndPrice> yearList = new ArrayList<>();
+        List<Product> productList = new ArrayList<>();
 
         Iterator<String> keys = variables.keys();
         while (keys.hasNext()) {
             JSONObject Number = variables.getJSONObject(keys.next());
 
-            String name = Number.getJSONObject("name").toString();
-            JSONArray valuesArray = Number.getJSONArray("values");
+            String name = Number.getString("name");
+            JSONArray priceList = Number.getJSONArray("values");
             JSONArray yearsArray = Number.getJSONArray("years");
 
-            List<Product> productList = new ArrayList<>();
+            List<YearAndPrice> yearAndPrices = new ArrayList<>();
 
             for (int i = 0; i < yearsArray.length(); i++) {
-
+                yearAndPrices.add(YearAndPrice.builder()
+                        .price(priceList.getInt(i))
+                        .year(yearsArray.getInt(i))
+                        .build());
             }
-            
-            YearAndPrice year = YearAndPrice.builder()
-                    .year(Integer.parseInt(name))
-                    .build();
-
+            productList.add(Product.builder()
+                    .nameProduct(name)
+                    .yearList(yearAndPrices)
+                    .build());
         }
+        productRepo.saveAll(productList);
     }
 }
