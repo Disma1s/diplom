7c9fdb82a49e5f85987e5ddc8e17626cadc55df6
Yurii <urarat271@gmail.com>
2021-04-14 12:37:51 +0300

    Add entity year

---
+++ src/main/java/ru/dismals/diplom/model/Year.java
@@ -0,0 +1,24 @@
+package ru.dismals.diplom.model;
+
+import lombok.Data;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+
+/**
+ * @author Yurii Tyshchuk
+ */
+@Entity
+@Data
+public class Year {
+    @Id
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name = "uuid", strategy = "uuid2")
+    @Column(unique = true)
+    private Long id;
+
+    private int year;
+
+    public Year() {
+    }
+}
