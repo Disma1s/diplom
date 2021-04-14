0ea23789ea313d6a04afa90fb450fdc9895be9b2
Disma1s <Dismals.q@yandex.ru>
2021-04-14 12:39:02 +0300

    Table product

---
+++ src/main/java/ru/dismals/diplom/model/Product.java
@@ -0,0 +1,23 @@
+package ru.dismals.diplom.model;
+
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.UUID;
+
+@Entity
+@Data
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+
+    private Long id;
+
+
+    private String nameProduct;
+    private int price;
+}
